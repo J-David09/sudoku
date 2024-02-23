d2d523e53102b66f519110bdeb801f9b450b4ca3
Juan David Uribe <juan_uribe82201@elpoli.edu.co>
2024-02-23 14:56:04 -0500

    Ajustes con manejo de matriz e inicializacion de server

--- src/Model/MatrixResult.java
+++ src/Model/MatrixResult.java
@@ -1,11 +1,48 @@
 package Model;
 
+import java.io.Serializable;
 import java.util.Arrays;
 
-public record MatrixResult(int[][][][] matrix, int steps) {
+public class MatrixResult implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private int[][][][] matrix;
+    private int result;
+
+    public MatrixResult(int[][][][] matrix, int result) {
+        this.matrix = matrix;
+        this.result = result;
+    }
+
+    public int[][][][] getMatrix() {
+        return matrix;
+    }
+
+    public void setMatrix(int[][][][] matrix) {
+        this.matrix = matrix;
+    }
+
+    public void printMatrix() {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print("| ");
+                for (int k = 0; k < matrix[i][j].length; k++) {
+                    for (int l = 0; l < matrix[i][j][k].length; l++) {
+                        System.out.print(matrix[i][j][k][l] + " ");
+                    }
+                    if (k < matrix[i][j].length - 1) {
+                        System.out.print("| ");
+                    }
+                }
+                System.out.print("|");
+                System.out.println();
+            }
+        }
+    }
 
     @Override
     public String toString() {
-        return "Steps to solve: " + steps + "\n" + Arrays.deepToString(matrix);
+        printMatrix();
+        return "MatrixResult";
     }
 }
--- src/Server/SudokuServer.java
+++ src/Server/SudokuServer.java
@@ -7,12 +7,15 @@ import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 
 public class SudokuServer {
-    public static void main(String []args) throws RemoteException
-    { Registry reg= LocateRegistry.createRegistry(1099);
-        SudokuImplement sudokuImplement=new SudokuImplement(0,0);
-        //Object Name
-        reg.rebind("Sudoku", sudokuImplement);
-        System.out.println("Server Started");
-
+    public static void main(String []args) throws RemoteException {
+        try {
+            Registry reg= LocateRegistry.createRegistry(1099);
+            SudokuImplement sudokuImplement=new SudokuImplement(0,0);
+            //Object Name
+            reg.rebind("Sudoku", sudokuImplement);
+            System.out.println("Server Started");
+        } catch (RemoteException e) {
+            System.err.println("Error starting the server: " + e.getMessage());
+        }
     }
 }
